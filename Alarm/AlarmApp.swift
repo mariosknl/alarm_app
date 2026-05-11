e3fe47450e7fe7c31dc8a1b8dcd605ef287d6135
Marios Kanellopoulos <mariosknl@outlook.com>
2026-05-11 12:05:50 +0300

    Initial Commit

---
+++ Alarm/AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmApp.swift
+//  Alarm
+//
+//  Created by Marios Kanellopoulos on 11/5/26.
+//
+
+import SwiftUI
+
+@main
+struct AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alarm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Alarm
+//
+//  Created by Marios Kanellopoulos on 11/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
