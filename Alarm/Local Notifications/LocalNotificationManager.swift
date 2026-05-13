a6e625e1ebef8bf194025f03ba35d26789dac35c
Marios Kanellopoulos <mariosknl@outlook.com>
2026-05-13 09:27:30 +0300

    Foreground Local Push Notification

--- Alarm/Local Notifications/LocalNotificationManager.swift
+++ Alarm/Local Notifications/LocalNotificationManager.swift
@@ -57,7 +57,10 @@ class LocalNotificationManager: NSObject, ObservableObject, UNUserNotificationCe
     
     override init() {
         super.init()
-        // TODO: Want alarm to go off when app is also active
+        // Necessary in order for the alarm
+        // to go off when app is in the
+        // foreground
+        notificationCenter.delegate = self
         
         // Alarm view model - persistence
         guard let data = UserDefaults
@@ -70,6 +73,28 @@ class LocalNotificationManager: NSObject, ObservableObject, UNUserNotificationCe
         }
         
         self.alarmViewModels = savedItems
+    }
+    
+    // Delegate function
+    func userNotificationCenter(
+        _center: UNUserNotificationCenter,
+        willPresent notification: UNNotification
+    ) async -> UNNotificationPresentationOptions {
+        
+        // After alarm goes off disable alarm
+        let notificationId = notification.request.identifier
+        
+        if let index = alarmViewModels.firstIndex(where: { $0.id == notificationId }) {
+            // disable alarm
+            alarmViewModels[index].alarmEnabled = false
+        }
+     
+        pendingAlarms = await notificationCenter.pendingNotificationRequests()
+        
+        return [
+            .sound,
+            .banner
+        ]
     }
     
     func getPendingAlarms() async {
